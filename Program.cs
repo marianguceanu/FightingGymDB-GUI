f86bf01575960c7bd802a9809ff37f2e11c60c96
Marian Guceanu <george.guceanu@stud.ubbcluj.ro>
2023-03-26 15:55:22 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace newDBMS
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            String connString = @"Data Source=DESKTOP-MQAIVDL\SQLEXPRESS;Initial Catalog=DBMS;Integrated Security=True";
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                Console.WriteLine("Succesful connection!");
+                conn.Close();
+            }
+        }
+    }
+}
